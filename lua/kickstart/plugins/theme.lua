3cefdb780347e2b01c6db00d780df0bdc9c692dd
aaarrti <artem.sereda.tub@gmail.com>
2026-03-11 16:43:36 +0100

    :construction:

--- lua/kickstart/plugins/theme.lua
+++ lua/kickstart/plugins/theme.lua
@@ -1,18 +1,47 @@
-return { -- You can easily change to a different colorscheme.
-  -- Change the name of the colorscheme plugin below, and then
-  -- change the command in the config to whatever the name of that colorscheme is.
-  --
-  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  'folke/tokyonight.nvim',
-  --'wojciechkepka/vim-github-dark',
+return {
+  'projekt0n/github-nvim-theme',
+  name = 'github-theme',
+  lazy = false,
   priority = 1000, -- Make sure to load this before all the other start plugins.
-  init = function()
-    -- Load the colorscheme here.
-    -- Like many other themes, this one has different styles, and you could load
-    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    vim.cmd.colorscheme 'tokyonight-night'
-    -- vim.cmd.colorscheme 'ghdark'
-    -- You can configure highlights by doing something like:
-    vim.cmd.hi 'Comment gui=none'
+  config = function()
+    require('github-theme').setup({
+      options = {
+        -- Compiled file's destination location
+        compile_path = vim.fn.stdpath 'cache' .. '/github-theme',
+        compile_file_suffix = '_compiled',
+        hide_end_of_buffer = true,
+        hide_nc_statusline = true,
+        transparent = false,
+        terminal_colors = true,
+        dim_inactive = false,
+        module_default = true,
+        styles = {
+          comments = 'NONE',
+          functions = 'NONE',
+          keywords = 'NONE',
+          variables = 'NONE',
+          conditionals = 'NONE',
+          constants = 'NONE',
+          numbers = 'NONE',
+          operators = 'NONE',
+          strings = 'NONE',
+          types = 'NONE',
+        },
+        inverse = {
+          match_paren = false,
+          visual = false,
+          search = false,
+        },
+        darken = {
+          floats = true,
+          sidebars = {
+            enable = true,
+            list = {},
+          },
+        },
+      },
+    })
+
+    vim.cmd 'colorscheme github_dark_default'
   end,
 }
